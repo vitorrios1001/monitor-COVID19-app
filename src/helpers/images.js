af17cf72d7aec6738e57e590f9b0ca90c23ab79c
Vitor Rios <vitor.neto@br.experian.com>
2020-05-29 21:18:59 -0300

    Helpers for import images

---
+++ src/helpers/images.js
@@ -0,0 +1,51 @@
+const IMG_STATES = {
+  AC: require('../assets/images/states/AC.png'),
+  AL: require('../assets/images/states/AL.png'),
+  AM: require('../assets/images/states/AM.png'),
+  AP: require('../assets/images/states/AP.png'),
+  BA: require('../assets/images/states/BA.png'),
+  CE: require('../assets/images/states/CE.png'),
+  DF: require('../assets/images/states/DF.png'),
+  ES: require('../assets/images/states/ES.png'),
+  GO: require('../assets/images/states/GO.png'),
+  MA: require('../assets/images/states/MA.png'),
+  MG: require('../assets/images/states/MG.png'),
+  MS: require('../assets/images/states/MS.png'),
+  MT: require('../assets/images/states/MT.png'),
+  PA: require('../assets/images/states/PA.png'),
+  PB: require('../assets/images/states/PB.png'),
+  PE: require('../assets/images/states/PE.png'),
+  PI: require('../assets/images/states/PI.png'),
+  PR: require('../assets/images/states/PR.png'),
+  RJ: require('../assets/images/states/RJ.png'),
+  RN: require('../assets/images/states/RN.png'),
+  RO: require('../assets/images/states/RO.png'),
+  RR: require('../assets/images/states/RR.png'),
+  RS: require('../assets/images/states/RS.png'),
+  SC: require('../assets/images/states/SC.png'),
+  SE: require('../assets/images/states/SE.png'),
+  SP: require('../assets/images/states/SP.png'),
+  TO: require('../assets/images/states/TO.png'),
+}
+
+const NAME_ICONS = {
+  death: 'death',
+  family: 'family',
+  recovered: 'recovered',
+  suspect: 'suspect',
+  update: 'update',
+  world: 'world',
+  brazil: 'brazil',
+}
+
+const ICONS = {
+  [NAME_ICONS.death]: require('../assets/images/icons/death.png'),
+  [NAME_ICONS.family]: require('../assets/images/icons/family.png'),
+  [NAME_ICONS.recovered]: require('../assets/images/icons/recovered.png'),
+  [NAME_ICONS.suspect]: require('../assets/images/icons/suspect.png'),
+  [NAME_ICONS.update]: require('../assets/images/icons/update.png'),
+  [NAME_ICONS.world]: require('../assets/images/icons/world.png'),
+  [NAME_ICONS.brazil]: require('../assets/images/icons/brazil.png'),
+}
+
+export { IMG_STATES, NAME_ICONS, ICONS }
