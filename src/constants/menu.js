cb3c3b1c8d69dc4aad6c74e0e9579d4031497903
Vitor Rios <vitor.neto@br.experian.com>
2020-05-29 21:18:21 -0300

    Adding constants

---
+++ src/constants/menu.js
@@ -0,0 +1,6 @@
+const MENU = [
+  { key: 'World', description: 'Mundo', nameIcon: 'world' },
+  { key: 'Brazil', description: 'Brasil', nameIcon: 'brazil' },
+]
+
+export { MENU }
---
+++ src/constants/routes.js
@@ -0,0 +1,24 @@
+import { Home, World, Brazil } from '../screens'
+
+const routes = [
+  {
+    name: 'Home',
+    description: 'Inicio',
+    showDefaultHeader: true,
+    component: Home,
+  },
+  {
+    name: 'World',
+    description: 'No mundo',
+    showDefaultHeader: true,
+    component: World,
+  },
+  {
+    name: 'Brazil',
+    description: 'Brasil',
+    showDefaultHeader: true,
+    component: Brazil,
+  },
+]
+
+export default routes
