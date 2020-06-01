06aa9785fab88bac819e3df2ae0c530a2e841bad
Vitor Rios <vitor.neto@br.experian.com>
2020-06-01 18:52:28 -0300

    Changing scrollList per FlatList with pagination

--- src/screens/Brazil/index.js
+++ src/screens/Brazil/index.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react'
 import {
   StyleSheet,
   SafeAreaView,
-  ScrollView,
   RefreshControl,
   View,
   Image,
   Dimensions,
+  FlatList,
 } from 'react-native'
 import { useSelector } from 'react-redux'
 import Loader from '../../components/Loader'
@@ -21,11 +21,15 @@ import { getAllStates } from '../../services/statesService'
 
 const WIDTH = Dimensions.get('window').width
 
+const perPage = 7
+
 const Brazil = () => {
+  const [list, setList] = useState([])
   const [loading, setLoading] = useState(false)
   const [refreshing, setRefresh] = useState(false)
   const [openModal, setOpenModal] = useState(false)
   const [stateSelected, setStateSelected] = useState({})
+  const [loadingMore, setLoadingMore] = useState(false)
   
   const states  = useSelector(state => state.states.data)
 
@@ -38,11 +42,21 @@ const Brazil = () => {
 
     await getAllStates(onRefresh)
 
+    setList(states.slice(0, perPage))
+
     if (onRefresh) {
       setRefresh(false)
     } else {
       setLoading(false)
     }
+  }
+
+  const loadMore = () => {
+    setLoadingMore(true)
+    const more = states.slice(list.length, list.length + 7)
+
+    setList([...list, ...more])
+    setLoadingMore(false)
   }
 
   useEffect(() => {
@@ -54,57 +68,67 @@ const Brazil = () => {
     setOpenModal(true)
   }
 
-  const renderCards = () => (
-    states.map((item, idx) => (
-      <Card
-        key={idx}
-        style={[stylesDefault.COL,styles.card]}
-        onPress={() => toggleCard(item)}
-      >
-        <View style={[stylesDefault.ROW]}>
-          <View>
-            <Image
-              style={styles.flagImage}
-              source={item.image}
-            />
+  const renderCard = ({ item }) => (
+    <Card
+      style={[stylesDefault.COL,styles.card]}
+      onPress={() => toggleCard(item)}
+    >
+      <View style={[stylesDefault.ROW]}>
+        <View>
+          <Image
+            style={styles.flagImage}
+            source={item.image}
+          />
+        </View>
+        <View style={[stylesDefault.COL, styles.content]}>
+          <View style={[stylesDefault.ROW, styles.item]}>
+            <Title>
+              {item.state}
+            </Title>
           </View>
-          <View style={[stylesDefault.COL, styles.content]}>
-            <View style={[stylesDefault.ROW, styles.item]}>
-              <Title>
-                {item.state}
-              </Title>
-            </View>
-            <View style={[stylesDefault.ROW, styles.item]}>
-              <Body style={styles.label}>
-                Casos:
-              </Body>
-              <Title>
-                {item.cases}
-              </Title>
-            </View>
-            <View style={[stylesDefault.ROW, styles.item]}>
-              <Body style={styles.label}>
-                Mortes:
-              </Body>
-              <Title>
-                {item.deaths}
-              </Title>
-            </View>
+          <View style={[stylesDefault.ROW, styles.item]}>
+            <Body style={styles.label}>
+              Casos:
+            </Body>
+            <Title>
+              {item.cases}
+            </Title>
+          </View>
+          <View style={[stylesDefault.ROW, styles.item]}>
+            <Body style={styles.label}>
+              Mortes:
+            </Body>
+            <Title>
+              {item.deaths}
+            </Title>
           </View>
         </View>
-      </Card>
-    ))
+      </View>
+    </Card>
   )
+
+  const renderFooter = () => {
+    if (!loadingMore) return null;
+
+    return <ActivityIndicator size="large" />
+  }
 
   return (
     <>
       <SafeAreaView style={styles.containerScreen} >
-        <ScrollView
-          style={styles.containerList}
-          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={() => loadData(true)} />}
-        >
-          {loading ? <Loader text="Carregando estados aguarde..." size={60} /> : renderCards()}
-        </ScrollView>
+        {
+          loading
+            ? <Loader text="Carregando estados aguarde..." /> 
+            : <FlatList
+                data={list}
+                keyExtractor={item => item.uf}
+                renderItem={renderCard}
+                onEndReachedThreshold={0.3}
+                ListFooterComponent={renderFooter}
+                onEndReached={loadMore}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={() => loadData(true)} />}
+              />
+        }
       </SafeAreaView>
       <Modal onClose={() => setOpenModal(false)} open={openModal}>
         <ContentInfo {...stateSelected} />
--- src/screens/World/index.js
+++ src/screens/World/index.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react'
 import {
   StyleSheet,
   SafeAreaView,
-  ScrollView,
   RefreshControl,
   View,
+  FlatList,
 } from 'react-native'
 import { useSelector } from 'react-redux'
 
@@ -18,8 +18,12 @@ import stylesDefault from '../../assets/styles/stylesDefault'
 
 import { NAME_ICONS } from '../../helpers/images'
 
+const perPage = 7
+
 const World = () => {
+  const [list, setList] = useState([])
   const [loading, setLoading] = useState(false)
+  const [loadingMore, setLoadingMore] = useState(false)
   const [refreshing, setRefresh] = useState(false)
   
   const countries = useSelector(state => state.countries.data)
@@ -33,6 +37,8 @@ const World = () => {
 
     await getAllCountries(onRefresh)
 
+    setList(countries.slice(0, perPage))
+
     if (onRefresh) {
       setRefresh(false)
     } else {
@@ -40,65 +46,86 @@ const World = () => {
     }
   }
 
+
+
+  const loadMore = () => {
+    setLoadingMore(true)
+    const more = countries.slice(list.length, list.length + 7)
+
+    setList([...list, ...more])
+    setLoadingMore(false)
+  }
+
   useEffect(() => {
     loadData()
   }, [])
 
-  const renderCards = () => (
-    countries.map((item, idx) => (
-      <Card key={idx} style={[stylesDefault.COL,styles.card]}>
+  const renderCard = ({ item }) => (
+    <Card style={[stylesDefault.COL,styles.card]}>
+      <View style={[stylesDefault.ROW, styles.rowAlign]}>
+        <Icon name={NAME_ICONS.world} size="small" />
+        <Title style={[styles.value]}>
+          {item.country}
+        </Title>
+      </View>
+      <View style={[stylesDefault.COL]}>
         <View style={[stylesDefault.ROW, styles.rowAlign]}>
-          <Icon name={NAME_ICONS.world} size="small" />
+          <Body>
+            Casos:
+          </Body>
           <Title style={[styles.value]}>
-            {item.country}
+            {item.cases}
           </Title>
         </View>
-        <View style={[stylesDefault.COL]}>
-          <View style={[stylesDefault.ROW, styles.rowAlign]}>
-            <Body>
-              Casos:
-            </Body>
-            <Title style={[styles.value]}>
-              {item.cases}
-            </Title>
-          </View>
-          <View style={[stylesDefault.ROW, styles.rowAlign]}>
-            <Body>
-              Confirmados:
-            </Body>
-            <Title style={[styles.value]}>
-              {item.confirmed}
-            </Title>
-          </View>
-          <View style={[stylesDefault.ROW, styles.rowAlign]}>
-            <Body>
-              Mortes:
-            </Body>
-            <Title style={[styles.value]}>
-              {item.deaths}
-            </Title>
-          </View>
-          <View style={[stylesDefault.ROW, styles.rowAlign]}>
-            <Body>
-              Recuperados:
-            </Body>
-            <Title style={[styles.value]}>
-              {item.recovered}
-            </Title>
-          </View>
+        <View style={[stylesDefault.ROW, styles.rowAlign]}>
+          <Body>
+            Confirmados:
+          </Body>
+          <Title style={[styles.value]}>
+            {item.confirmed}
+          </Title>
         </View>
-      </Card>
-    ))
+        <View style={[stylesDefault.ROW, styles.rowAlign]}>
+          <Body>
+            Mortes:
+          </Body>
+          <Title style={[styles.value]}>
+            {item.deaths}
+          </Title>
+        </View>
+        <View style={[stylesDefault.ROW, styles.rowAlign]}>
+          <Body>
+            Recuperados:
+          </Body>
+          <Title style={[styles.value]}>
+            {item.recovered}
+          </Title>
+        </View>
+      </View>
+    </Card>
   )
+    
+  const renderFooter = () => {
+    if (!loadingMore) return null;
+
+    return <ActivityIndicator size="large" />
+  }
 
   return (
     <SafeAreaView>
-      <ScrollView
-        style={styles.container}
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={() => loadData(true)} />}
-      >
-        {loading ? <Loader text="Carregando países aguarde..." /> : renderCards()}
-      </ScrollView>
+      {
+        loading
+          ? <Loader text="Carregando países aguarde..." /> 
+          : <FlatList
+              data={list}
+              keyExtractor={item => item.country}
+              renderItem={renderCard}
+              onEndReachedThreshold={0.3}
+              ListFooterComponent={renderFooter}
+              onEndReached={loadMore}
+              refreshControl={<RefreshControl refreshing={refreshing} onRefresh={() => loadData(true)} />}
+            />
+      }
     </SafeAreaView>
   )
 }
